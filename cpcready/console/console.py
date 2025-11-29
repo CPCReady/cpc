12b34167eb73d2e16c925c4c83631eaf91da1fca
destroyer <destroyer.dcf@gmail.com>
2025-11-29 14:26:10 +0100

    cambiamos licencia

--- cpcready/console/console.py
+++ cpcready/console/console.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2025 David CH.F (destroyer)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """
 CPCReady Interactive Console with bottom toolbar
 """
@@ -272,7 +287,7 @@ def console():
     # Main loop
     while True:
         try:
-            # Obtener directorio actual para el prompt
+            # Obtener directorio currente para el prompt
             import os
             current_dir = os.getcwd()
             home = os.path.expanduser("~")
